d6f9548c26dbf920de236aa9fcf361817705fb4e
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-27 09:13:01 -0300

    sass works

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
