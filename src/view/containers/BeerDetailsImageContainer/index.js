c4a26268b51af985105e230ed86926bba8284d8c
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-30 11:28:38 -0300

    details ok

--- src/view/containers/BeerDetailsImageContainer/index.js
+++ src/view/containers/BeerDetailsImageContainer/index.js
@@ -1,90 +1,25 @@
 import React from "react";
+import { connect } from "react-redux";
+
 import BeerDetailsImage from "../../components/BeerDetailsImage";
 
-const BeerDetailsImageContainer = () => {
-  const { imageUrl, name } = BEER;
+const BeerDetailsImageContainer = props => {
+  const { imageUrl, name, isLoadingSelectedBeer, selectedBeerId } = props;
 
   return (
     <>
-      <BeerDetailsImage imageUrl={imageUrl} name={name} />
+      {!isLoadingSelectedBeer && selectedBeerId && (
+        <BeerDetailsImage imageUrl={imageUrl} name={name} />
+      )}
     </>
   );
 };
 
-export default BeerDetailsImageContainer;
+const mapStateToProps = state => ({
+  selectedBeerId: state.beers.selectedBeer.id,
+  imageUrl: state.beers.selectedBeer.imageUrl,
+  isLoadingSelectedBeer: state.beers.isLoadingSelectedBeer,
+  error: state.beers.error
+});
 
-const BEER = {
-  id: 1,
-  name: "Buzz",
-  tagline: "A Real Bitter Experience.",
-  firstBrewed: "09/2007",
-  description:
-    "A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once.",
-  imageUrl: "https://images.punkapi.com/v2/keg.png",
-  abv: 4.5,
-  ibu: 60,
-  target_fg: 1010,
-  target_og: 1044,
-  ebc: 20,
-  srm: 10,
-  ph: 4.4,
-  attenuationLevel: 75,
-  volume: { value: 20, unit: "litres" },
-  boilVolume: { value: 25, unit: "litres" },
-  method: {
-    mash_temp: [{ temp: { value: 64, unit: "celsius" }, duration: 75 }],
-    fermentation: { temp: { value: 19, unit: "celsius" } },
-    twist: null
-  },
-  ingredients: {
-    malt: [
-      {
-        name: "Maris Otter Extra Pale",
-        amount: { value: 3.3, unit: "kilograms" }
-      },
-      { name: "Caramalt", amount: { value: 0.2, unit: "kilograms" } },
-      { name: "Munich", amount: { value: 0.4, unit: "kilograms" } }
-    ],
-    hops: [
-      {
-        name: "Fuggles",
-        amount: { value: 25, unit: "grams" },
-        add: "start",
-        attribute: "bitter"
-      },
-      {
-        name: "First Gold",
-        amount: { value: 25, unit: "grams" },
-        add: "start",
-        attribute: "bitter"
-      },
-      {
-        name: "Fuggles",
-        amount: { value: 37.5, unit: "grams" },
-        add: "middle",
-        attribute: "flavour"
-      },
-      {
-        name: "First Gold",
-        amount: { value: 37.5, unit: "grams" },
-        add: "middle",
-        attribute: "flavour"
-      },
-      {
-        name: "Cascade",
-        amount: { value: 37.5, unit: "grams" },
-        add: "end",
-        attribute: "flavour"
-      }
-    ],
-    yeast: "Wyeast 1056 - American Ale™"
-  },
-  foodPairing: [
-    "Spicy chicken tikka masala",
-    "Grilled chicken quesadilla",
-    "Caramel toffee cake"
-  ],
-  brewers_tips:
-    "The earthy and floral aromas from the hops can be overpowering. Drop a little Cascade in at the end of the boil to lift the profile with a bit of citrus.",
-  contributed_by: "Sam Mason <samjbmason>"
-};
+export default connect(mapStateToProps)(BeerDetailsImageContainer);
