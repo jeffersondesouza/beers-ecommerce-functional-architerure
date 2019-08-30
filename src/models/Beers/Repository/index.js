1cbf81ba2762fcfec085edf8040e68f308802d50
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-30 10:35:57 -0300

    load beer

--- src/models/Beers/Repository/index.js
+++ src/models/Beers/Repository/index.js
@@ -3,6 +3,12 @@ const loadBeers = page => ({
   method: "GET"
 });
 
+const loadBeer = page => ({
+  path: `https://api.punkapi.com/v2/beers?page=${page}&per_page=10`,
+  method: "GET"
+});
+
 export default {
-  loadBeers
+  loadBeers,
+  loadBeer
 };
