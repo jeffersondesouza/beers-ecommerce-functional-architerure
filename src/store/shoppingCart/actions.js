cb7a9f1f161f3f9cb7b00427c321da7a6fb41bb4
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-08-31 15:31:50 -0300

    shoppingCart

---
+++ src/store/shoppingCart/actions.js
@@ -0,0 +1,40 @@
+import Types from "./constants";
+
+const loadBeersRequest = page => ({
+  type: Types.LOAD_BEERS_REQUEST,
+  payload: page
+});
+
+const loadBeersSuccess = payload => ({
+  type: Types.LOAD_BEERS_SUCCESS,
+  payload
+});
+
+const loadBeersFailure = payload => ({
+  type: Types.LOAD_BEERS_FAILURE,
+  payload
+});
+
+const loadBeerRequest = payload => ({
+  type: Types.LOAD_BEER_REQUEST,
+  payload
+});
+
+const loadBeerSuccess = payload => ({
+  type: Types.LOAD_BEER_SUCCESS,
+  payload
+});
+
+const loadBeerFailure = payload => ({
+  type: Types.LOAD_BEER_FAILURE,
+  payload
+});
+
+export default {
+  loadBeersRequest,
+  loadBeersSuccess,
+  loadBeersFailure,
+  loadBeerRequest,
+  loadBeerSuccess,
+  loadBeerFailure
+};
