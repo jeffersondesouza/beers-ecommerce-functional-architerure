bb73c71290e57c215afded7033f5dad51e2afff9
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2019-09-03 17:35:48 -0300

    monads ok

--- src/store/beers/selectors/getBeersList.js
+++ src/store/beers/selectors/getBeersList.js
@@ -1,1 +1,7 @@
-export default state => state.beers.beersList;
+import Maybe from "../../utils/maybe";
+
+export default state =>
+  Maybe.of(state)
+    .map(st => st.beers)
+    .map(beers => beers.beersList)
+    .get([]);
